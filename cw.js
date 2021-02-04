d8a93fedf0ecb39f9ff15c21f1c8d9b0b522ebcb
i-doshechnikow <77616522+i-doshechnikow@users.noreply.github.com>
2021-02-04 17:49:26 +0300

    Add files via upload

--- cw.js
+++ cw.js
@@ -5,3 +5,20 @@ function fireFight(s) {
 }
 
 console.log(fireFight(str));
+
+// Given the triangle of consecutive odd numbers:
+
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
+// Calculate the row sums of this triangle from the row index (starting at index 1) e.g.:
+
+function rowSumOddNumbers(n) {
+    return n * n * n;
+}
+
+console.log(rowSumOddNumbers(5));
+
